752c74f2e1186834b4a05d3901621c503b939b66
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2022-02-03 00:00:40 +0530

    Add files via upload

---
+++ Python/007-forLoop.py
@@ -0,0 +1,23 @@
+list = [1,2,3,4,5]
+for i in list:
+    print(i, end=" ")
+print()
+
+tup = ('a','b','c','d')
+for str in tup:
+    print(str, end=" ")
+print()
+
+string = "Python"
+for char in string:
+    print(char, end=" ")
+print()
+
+dict = {'Sem1': 90, 'Sem2': 95, 'Btech':(90, 98)}
+for pair in dict:
+    print(pair,end=" ")
+    print(dict[pair],end=" ")
+print()
+
+for i in range(1,10):
+    print(i,end=" ")
