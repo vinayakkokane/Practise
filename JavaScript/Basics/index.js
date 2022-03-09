3996b2d3bd2640e642e091f22f5a1f08d12b8010
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2022-03-09 19:33:28 +0530

    Add files via upload

---
+++ JavaScript/Basics/index.js
@@ -0,0 +1,24 @@
+console.log("Okay Good");
+
+const num1 = 20;
+const num2 = 30;
+console.log(num1+num2);
+
+var isComing = new Boolean(true);
+
+Person = {
+    name : 'Vinayak',
+    age : 21
+};
+
+console.log(Person.name + "\nhe's coming? : " + isComing);
+
+let array = [1,2,3,4];
+console.log(array);
+
+function multiplication(n1,n2){
+    return n1*n2;
+}
+
+const res = multiplication(2,3);
+console.log(res);
